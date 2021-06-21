6caf1688a734d3da6c19fe38967a64284c3712af
abdohantash <62960904+abdohantash@users.noreply.github.com>
2021-06-21 16:52:14 +0200

    test
    test

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+// Hello.
+//
+// This is The Scripts used for ___________ Theme
+//
+//
+
+function main() {
+
+    (function () {
+       'use strict';
+    
+       /* ==============================================
+          Testimonial Slider
+          =============================================== */ 
+    
+          $('a.page-scroll').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html,body').animate({
+                  scrollTop: target.offset().top - 40
+                }, 900);
+                return false;
+              }
+            }
+          });
+    
+    }());
+    
+    
+    }
+    main();
+
+
